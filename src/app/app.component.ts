170ea20fb50d48a86074536afd8fa2448d5da6ac
zapatoada <32368237+zapatoada@users.noreply.github.com>
2019-02-13 23:44:44 -0500

    better validations for DOB

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,29 @@
 import { Component, OnInit } from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from '@angular/forms';
-import {Make, allMakes} from './Make';
+import { FormBuilder, FormGroup, Validators, AbstractControl, ValidatorFn } from '@angular/forms';
+import { Make, allMakes } from './Make';
 
+function dateMin(min: Date): ValidatorFn {
+  return (control: AbstractControl): { [key: string]: boolean } | null => {
+    let val = new Date(control.value);
+    if (control.value !== undefined && (isNaN(val.getTime()) || val < min )) {
+      return { 'dateMin': true };
+    }
+    return null;
+  };
+}
+
+function dateMax(max: Date): ValidatorFn {
+  return (control: AbstractControl): { [key: string]: boolean } | null => {
+    let val = new Date(control.value);
+    if (control.value !== undefined && (isNaN(val.getTime()) || val > max)) {
+      return { 'dateMax': true };
+    }
+    return null;
+  };
+}
+
+const endOfToday = new Date();
+endOfToday.setHours(23, 59, 59, 999);
 
 @Component({
   selector: 'app-root',
@@ -10,27 +32,26 @@ import {Make, allMakes} from './Make';
 })
 
 
-
 export class AppComponent implements OnInit {
   isLinear = false;
   firstFormGroup: FormGroup;
   secondFormGroup: FormGroup;
   makes: Make[] = allMakes;
-  maxModelYear: number = new Date().getFullYear()+1;
+  maxModelYear: number = new Date().getFullYear() + 1;
 
-  constructor(private _formBuilder: FormBuilder) {}
+  constructor(private _formBuilder: FormBuilder) { }
 
   ngOnInit() {
     this.firstFormGroup = this._formBuilder.group({
-      firstNameCtrl: ['', Validators.compose([Validators.required, Validators.maxLength(25)])],
-      lastNameCtrl: ['', Validators.compose([Validators.required, Validators.maxLength(25)])],
-      dobCtrl: ['', Validators.required]
+      firstNameCtrl: ['', [Validators.required, Validators.maxLength(25)]],
+      lastNameCtrl: ['', [Validators.required, Validators.maxLength(25)]],
+      dobCtrl: ['', [Validators.required, dateMin(new Date(1900, 1, 1)), dateMax(endOfToday)]]
     });
     this.secondFormGroup = this._formBuilder.group({
-      vehicleYearCtrl: ['', Validators.compose([Validators.required, Validators.min(1970),Validators.max(this.maxModelYear)])],
+      vehicleYearCtrl: ['', [Validators.required, Validators.min(1970), Validators.max(this.maxModelYear)]],
       vehicleMakeCtrl: ['', Validators.required]
     });
-    
+
   }
   title = 'compare-example';
 }
